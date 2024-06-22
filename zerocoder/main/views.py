cba7e821abc0e71b9bbd08bb168b8039a1437a3b
VictoriaZeltser <vikaz1976@gmail.com>
2024-06-22 17:40:49 +0300

    new code DJ01

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Djando</h1>")
+    return HttpResponse("<h1>Добро пожаловать на мою страницу</h1>")
 
 
 
